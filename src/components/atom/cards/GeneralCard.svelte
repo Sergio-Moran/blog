7d1a99a6cc70bfa41d71a62e3fa444930577ee29
Sergio Moran <sergioMoran@gmail.com>
2023-07-08 09:20:34 -0600

    Create BlogCard y Person icons

--- src/components/atom/cards/GeneralCard.svelte
+++ src/components/atom/cards/GeneralCard.svelte
@@ -1,22 +1,29 @@
 <script>
-  import Category from "../icon/Category.svelte";
-
+  export let title = "";
+  export let body = "";
+  export let principalBadge = "";
+  export let badgeOne = "";
+  export let badgeTwo = "";
 </script>
 
-<div class="card bordered w-max bg-white/50 shadow-xl mt-4 max-w-md md:max-w-2xl">
+<div
+  class="card bordered w-max bg-white/50 shadow-xl mt-4 max-w-md md:max-w-2xl"
+>
   <div class="card-body">
     <h2 class="card-title">
-      Title
-      <div class="badge bg-green-300 border-green-600">Reactions: 7</div>
+      {title}
+      <div class="badge bg-green-300 border-green-600">{principalBadge}</div>
     </h2>
     <p>
-      Body
+      {body}
     </p>
     <div class="card-actions justify-end">
-      <div class="badge badge-lg badge-ghost">Autor: A</div>
+      <div class="badge badge-lg badge-ghost">
+        {badgeOne}
+      </div>
       <div class="badge badge-lg bg-sky-300 border-sky-600">
-        <Category />
-        Category</div>
+        {badgeTwo}
+      </div>
     </div>
   </div>
 </div>
---
+++ src/components/organisms/BlogCard.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Category from "../atom/icon/Category.svelte";
+  import Person from "../atom/icon/Person.svelte";
+
+
+</script>
+
+<div class="card bordered w-max bg-white/50 shadow-xl mt-4 max-w-md md:max-w-2xl">
+    <div class="card-body">
+      <h2 class="card-title">
+        Title
+        <div class="badge bg-green-300 border-green-600">Reactions: 7</div>
+      </h2>
+      <p>
+        Body
+      </p>
+      <div class="card-actions justify-end">
+        <div class="badge badge-lg badge-ghost">
+          <Person />
+          Autor: A</div>
+        <div class="badge badge-lg bg-sky-300 border-sky-600">
+          <Category />
+          Category</div>
+      </div>
+    </div>
+  </div>
