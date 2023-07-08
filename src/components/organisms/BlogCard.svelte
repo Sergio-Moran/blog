1bf7af1c1e1aeb866cffd0c717dee724a0270cb7
Sergio Moran <sergioMoran@gmail.com>
2023-07-08 09:22:25 -0600

    Modify BlogCard

--- src/components/organisms/BlogCard.svelte
+++ src/components/organisms/BlogCard.svelte
@@ -1,26 +1,33 @@
 <script>
   import Category from "../atom/icon/Category.svelte";
   import Person from "../atom/icon/Person.svelte";
-
-
+  export let title = "";
+  export let body = "";
+  export let principalBadge = "";
+  export let badgeOne = "";
+  export let badgeTwo = "";
 </script>
 
-<div class="card bordered w-max bg-white/50 shadow-xl mt-4 max-w-md md:max-w-2xl">
-    <div class="card-body">
-      <h2 class="card-title">
-        Title
-        <div class="badge bg-green-300 border-green-600">Reactions: 7</div>
-      </h2>
-      <p>
-        Body
-      </p>
-      <div class="card-actions justify-end">
-        <div class="badge badge-lg badge-ghost">
-          <Person />
-          Autor: A</div>
-        <div class="badge badge-lg bg-sky-300 border-sky-600">
-          <Category />
-          Category</div>
+<div
+  class="card bordered w-max bg-white/50 shadow-xl mt-4 max-w-md md:max-w-2xl"
+>
+  <div class="card-body">
+    <h2 class="card-title">
+      {title}
+      <div class="badge bg-green-300 border-green-600">{principalBadge}</div>
+    </h2>
+    <p>
+      {body}
+    </p>
+    <div class="card-actions justify-end">
+      <div class="badge badge-lg badge-ghost">
+        <Person />
+        {badgeOne}
+      </div>
+      <div class="badge badge-lg bg-sky-300 border-sky-600">
+        <Category />
+        {badgeTwo}
       </div>
     </div>
   </div>
+</div>
